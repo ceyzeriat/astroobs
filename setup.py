5debdd7a5c278c43d69737a762695200b12323ec
Guillaume <guillaume.schworer@gmail.com>
2015-04-12 16:41:57 +0200

    upd setup.py to make astroobs install-able with pip

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 setup(
   name = 'astroobs',
   packages = ['astroobs'],
-  version = '1.0',
+  version = '1.0.1',
   description = 'Provides astronomy ephemeris to plan telescope observations',
   author = 'Guillaume Schworer',
   author_email = 'guillaume.schworer@obspm.fr',
   url = 'https://github.com/ceyzeriat/astroobs',
   download_url = 'https://github.com/ceyzeriat/astroobs',
   keywords = ['astronomy','ephemeris','pyephem','iobserve','observatory','telescope','observer','target','airmass'], # arbitrary keywords
-  classifiers = ['Development Status :: 2 - Pre-Alpha','Environment :: Console','Intended Audience :: Education','Intended Audience :: Science/Research','License :: OSI Approved :: MIT License','Operating System :: Unix','Programming Language :: Python :: 2.7','Topic :: Documentation :: Sphinx', 'Topic :: Scientific/Engineering :: Physics']
+  classifiers = ['Development Status :: 2 - Pre-Alpha','Environment :: Console','Intended Audience :: Education','Intended Audience :: Science/Research','License :: OSI Approved :: MIT License','Operating System :: Unix','Programming Language :: Python :: 2.7','Topic :: Documentation :: Sphinx', 'Topic :: Scientific/Engineering :: Physics'],
+  install_requires=['pyephem','numpy','pytz','datetime']
 )

cea9891693a0430ebe0239df9f30ae59026541b4
Guillaume <guillaume.schworer@gmail.com>
2015-04-12 16:37:16 +0200

    upd gitignore

---
+++ astroobs/__init__.py
@@ -0,0 +1,7 @@
+"""
+Provides astronomy ephemeris to plan telescope observations
+"""
+
+__all__ = ['astroobs'] # and all new files that should appear in "misc." listing
+
+import astroobs
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'astroobs',
+  packages = ['astroobs'],
+  version = '1.0',
+  description = 'Provides astronomy ephemeris to plan telescope observations',
+  author = 'Guillaume Schworer',
+  author_email = 'guillaume.schworer@obspm.fr',
+  url = 'https://github.com/ceyzeriat/astroobs',
+  download_url = 'https://github.com/ceyzeriat/astroobs',
+  keywords = ['astronomy','ephemeris','pyephem','iobserve','observatory','telescope','observer','target','airmass'], # arbitrary keywords
+  classifiers = ['Development Status :: 2 - Pre-Alpha','Environment :: Console','Intended Audience :: Education','Intended Audience :: Science/Research','License :: OSI Approved :: MIT License','Operating System :: Unix','Programming Language :: Python :: 2.7','Topic :: Documentation :: Sphinx', 'Topic :: Scientific/Engineering :: Physics']
+)

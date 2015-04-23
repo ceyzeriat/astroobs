f1d4135295f01a9ef61c6c7f2051a05113a08ef2
Guillaume <guillaume.schworer@gmail.com>
2015-04-23 18:19:27 +0200

    beta version

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ from distutils.core import setup
 setup(
     name = 'astroobs',
     packages = ['astroobs'],
-    version = '1.1.3',
+    version = '1.2',
     description = 'Provides astronomy ephemeris to plan telescope observations',
     author = 'Guillaume Schworer',
     author_email = 'guillaume.schworer@obspm.fr',
     url = 'https://github.com/ceyzeriat/astroobs',
     download_url = 'https://github.com/ceyzeriat/astroobs/tree/master/dist',
     keywords = ['astronomy','ephemeris','pyephem','iobserve','observatory','telescope','observer','target','airmass'], # arbitrary keywords
-    classifiers = ['Development Status :: 2 - Pre-Alpha','Environment :: Console','Intended Audience :: Education','Intended Audience :: Science/Research','License :: OSI Approved :: MIT License','Operating System :: Unix','Programming Language :: Python :: 2.7','Topic :: Documentation :: Sphinx', 'Topic :: Scientific/Engineering :: Physics'],
+    classifiers = ['Development Status :: 4 - Beta','Environment :: Console','Intended Audience :: Education','Intended Audience :: Science/Research','License :: OSI Approved :: MIT License','Operating System :: Unix','Programming Language :: Python :: 2.7','Topic :: Documentation :: Sphinx', 'Topic :: Scientific/Engineering :: Physics'],
     install_requires=['pyephem','pytz'],
     package_data={'': ['obsData.txt']},
     include_package_data=True

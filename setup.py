e416b583013bb2a07f42d6d1602f0ee26b5203e6
Guillaume <guillaume.schworer@gmail.com>
2015-05-23 17:43:08 +0200

    badges

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from distutils.core import setup
 setup(
     name = 'astroobs',
     packages = ['astroobs'],
-    version = '1.3.4',
+    version = '1.3.5',
     description = 'Provides astronomy ephemeris to plan telescope observations',
     author = 'Guillaume Schworer',
     author_email = 'guillaume.schworer@obspm.fr',
@@ -20,7 +20,7 @@ setup(
     download_url = 'https://github.com/ceyzeriat/astroobs/tree/master/dist',
     keywords = ['astronomy', 'ephemeris', 'pyephem', 'iobserve', 'observatory', 'telescope', 'observer', 'target', 'airmass'], # arbitrary keywords
     classifiers = ['Development Status :: 4 - Beta','Environment :: Console','Intended Audience :: Education','Intended Audience :: Science/Research','License :: OSI Approved :: MIT License','Operating System :: Unix','Programming Language :: Python :: 2.7','Topic :: Documentation :: Sphinx', 'Topic :: Scientific/Engineering :: Physics'],
-    install_requires=[],
+#    install_requires=[],
     package_data={'': ['obsData.txt']},
     include_package_data=True
 )
